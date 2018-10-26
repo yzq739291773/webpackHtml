d80127683037b7cd305c8e12d37018a9b98a93a0
yzq <739291773@qq.com>
2018-10-26 16:33:49 +0800

    less打包后的分离，同css分离

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -56,16 +56,18 @@ module.exports = {
             },
             {
                 test: /\.less$/,
-                use: [{
-                        loader: "style-loader" // creates style nodes from JS strings
-                    },
-                    {
-                        loader: "css-loader" // translates CSS into CommonJS
-                    },
-                    {
-                        loader: "less-loader" // compiles Less to CSS
-                    }
-                ]
+                use: extractTextPlugin.extract({
+                    use: [{
+                            loader: "css-loader" // translates CSS into CommonJS
+                        },
+                        {
+                            loader: "less-loader" // compiles Less to CSS
+                        }
+                    ],
+                    // use style-loader in development
+                    fallback: "style-loader"
+                })
+
             }
         ]
     },
