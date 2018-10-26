5f903a93a721be916add1bcd662de3fd4b863bca
yzq <739291773@qq.com>
2018-10-26 16:02:41 +0800

    增加less功能

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -53,6 +53,19 @@ module.exports = {
             {
                 test: /\.(htm|html)$/i,
                 use: ['html-withimg-loader']
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                        loader: "style-loader" // creates style nodes from JS strings
+                    },
+                    {
+                        loader: "css-loader" // translates CSS into CommonJS
+                    },
+                    {
+                        loader: "less-loader" // compiles Less to CSS
+                    }
+                ]
             }
         ]
     },
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import $ from 'jquery'
 import css from './css/index.css'
+import less from './less/index.less'
 
 console.log("11111")
 console.log('环境', process.env)
